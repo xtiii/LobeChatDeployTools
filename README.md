80a5cd4c6853457a28e90c6cf242690e8c43fbe4
SoTime <zlt990828@126.com>
2024-11-29 12:58:05 +0800

    添加国内源，并通过Ping自动选择，优化根目录判断逻辑

--- README.md
+++ README.md
@@ -1,24 +1,28 @@
 # lcdt - LobeChatDeployTools
+
 基于 [lobehub/lobe-chat](https://github.com/lobehub/lobe-chat) 的本地，无 Docker 部署方式
 
 ## 功能演示
+
 ![tools](https://raw.githubusercontent.com/xtiii/LobeChatDeployTools/main/Img/tools.jpg)
 
 ## 使用方法：
+
 ### 1. 首先安装 Node.js，以及 bun (依次执行以下命令)
+
+以下演示 nvm 方式安装 Node.js
+[更多安装方式点此查看](https://nodejs.org/en/download/package-manager)
+
 ```shell
-# installs fnm (Fast Node Manager)
-curl -fsSL https://fnm.vercel.app/install | bash
+# installs nvm (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 
-# activate fnm
-source ~/.bashrc
+# 注意：这个时候请查看输出信息，并执行输出信息内的命令
 
-# download and install Node.js
-fnm use --install-if-missing 22
-
+# download and install Node.js (you may need to restart the terminal)
+nvm install 22
 # verifies the right Node.js version is in the environment
 node -v # should print `v22.11.0`
-
 # verifies the right npm version is in the environment
 npm -v # should print `10.9.0`
 
@@ -30,6 +34,15 @@ bun -v
 ```
 
 ### 2. 运行脚本
+
+国内源
+
+```shell
+wget -O lcdt.sh https://gitee.com/SoTime/LobeChatDeployTools/raw/main/lcdt.sh && chmod +x lcdt.sh && ./lcdt.sh
+```
+
+国外源
+
 ```shell
 wget -O lcdt.sh https://raw.githubusercontent.com/xtiii/LobeChatDeployTools/main/lcdt.sh && chmod +x lcdt.sh && ./lcdt.sh
 ```
--- lcdt.sh
+++ lcdt.sh
@@ -4,6 +4,12 @@ clear
 
 set -e # 如果任何命令返回非零值，则退出脚本
 
+# 检查当前用户是否为 root 用户
+if ! [ "$(id -u)" -eq 0 ]; then
+  echo "❌ 请使用 Root 用户运行！"
+  exit 1
+fi
+
 # 检查 node.js
 if [ "$(command -v node)" ]; then
   NODE_VERSION=$(node -v)
@@ -31,7 +37,7 @@ fi
 # 检查 git
 if ! [ "$(command -v git)" ]; then
   echo "❌ git 未安装！正在为您安装..."
-  sudo apt-get install git -y
+  sudo apt install git -y
 fi
 
 # 获取脚本所在的完整原始路径
@@ -42,7 +48,10 @@ SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 SCRIPT_NAME=$(basename "$SCRIPT_PATH")
 
 # 定义仓库地址
-STOREHOSE_URL="https://raw.githubusercontent.com/xtiii/LobeChatDeployTools/main"
+# Gitee源
+CN_STOREHOSE_URL="https://gitee.com/SoTime/LobeChatDeployTools/raw/main"
+# GitHub源
+EN_STOREHOSE_URL="https://raw.githubusercontent.com/xtiii/LobeChatDeployTools/main"
 
 # 定义 src 文件夹名
 SRC_DIR="$SCRIPT_DIR/src"
@@ -51,9 +60,46 @@ PACKAGE_FILE="$SCRIPT_DIR/package.json"
 # 定义部署工具的存放目录名
 DEPLOYTOOLS_DIR="LobeChatDeployTools"
 
+# 通过 Ping 选择国内外源
+url_select() {
+  GITEE="gitee.com"
+  GITHUB="raw.githubusercontent.com"
+
+  # Ping 的延迟限制
+  TIMEOUT=0.5
+
+  # 异步执行Ping测试第一个URL并获取延迟
+  (ping -W $TIMEOUT -c 3 $GITEE | tail -1 | awk -F '/' '{print $5}' >latency1.txt) &
+
+  # 异步执行Ping测试第二个URL并获取延迟
+  (ping -W $TIMEOUT -c 3 $GITHUB | tail -1 | awk -F '/' '{print $5}' >latency2.txt) &
+
+  echo -e "正在为您选择源..."
+  # 等待所有异步进程完成
+  wait
+
+  # 读取延迟值
+  GITEE_LATENCY=$(cat latency1.txt)
+  GITHUB_LATENCY=$(cat latency2.txt)
+
+  # 清理临时文件
+  rm -rf latency1.txt latency2.txt
+
+  # 比较两个URL的平均延迟
+  if (($(echo "$GITEE_LATENCY < $GITHUB_LATENCY" | bc -l))); then
+    LOCATION="Gitee"
+    STOREHOSE_URL=$CN_STOREHOSE_URL
+  else
+    LOCATION="GitHub"
+    STOREHOSE_URL=$EN_STOREHOSE_URL
+  fi
+}
+
+# 自动选择国内外源
+url_select
+
 # 安装依赖
 install() {
-  echo "开始安装依赖"
   NODE_FILE="./node_modules"
   if [ -d "$NODE_FILE" ]; then
     rm -rf "$NODE_FILE"
@@ -63,7 +109,6 @@ install() {
 
 # 构建程序
 build() {
-  echo "开始构建程序"
   echo "注意：如果最后出现 db:migrate 相关错误，无需理会，执行本程序提供的 数据库迁移 即可！"
   NEXT_FILE="./.next"
   if [ -d "$NEXT_FILE" ]; then
@@ -77,24 +122,25 @@ build() {
 
 # 运行程序
 run() {
-  echo "开始运行程序"
   # 默认端口
   DEFAULT_PORN=3210
-  read -pr "请输入指定端口号(1 - 65535)，默认：${DEFAULT_PORN}: " PORN
+  read -rp "请输入指定端口号(1 - 65535)，默认：$DEFAULT_PORN：" PORN
   # 如果用户未输入内容，使用默认端口
-  if [ -z "$port" ]; then
+  if [ -z "$PORN" ]; then
     PORN=$DEFAULT_PORN
   fi
+  echo "当前设置的端口号为：$PORN"
   bun run start -H 0.0.0.0 -p "$PORN"
 }
 
 # 更新程序
 update() {
-  echo "开始更新程序，这可能需要一些时间！"
   OUTPUT=$(git pull origin main)
   if [[ $OUTPUT == *"up to date"* || $OUTPUT == *"最新"* ]]; then
+    clear
     echo "🎉 已经是最新版本！"
   else
+    clear
     echo "检测到更新，开始更新并自动部署！"
     install
     build
@@ -104,11 +150,13 @@ update() {
 # 数据库操作
 goto_db() {
   SCRIPT_URL="$STOREHOSE_URL/$DEPLOYTOOLS_DIR/$DB_SCRIPT_NAME"
+  # 判断数据库操作工具目录是否存在
   if [ ! -d ./$DEPLOYTOOLS_DIR ]; then
     mkdir -p ./$DEPLOYTOOLS_DIR
   fi
+  # 判断数据库操作工具是否存在
   if [ ! -f ./$DEPLOYTOOLS_DIR/$DB_SCRIPT_NAME ]; then
-    wget -q -O "./$DEPLOYTOOLS_DIR/$DB_SCRIPT_NAME" "$SCRIPT_URL"
+    wget -O "./$DEPLOYTOOLS_DIR/$DB_SCRIPT_NAME" "$SCRIPT_URL"
     chmod +x "./$DEPLOYTOOLS_DIR/$DB_SCRIPT_NAME"
   fi
   ./$DEPLOYTOOLS_DIR/$DB_SCRIPT_NAME || true
@@ -116,15 +164,17 @@ goto_db() {
 
 # 更新脚本
 update_script() {
-  SCRIPT_URL="$STOREHOSE_URL/LobeChatDeployTools.sh"
+  SCRIPT_URL="$STOREHOSE_URL/lcdt.sh"
   wget -O "$SCRIPT_NAME" "$SCRIPT_URL"
   sudo ln -sf "$SCRIPT_DIR/$SCRIPT_NAME" /usr/local/bin/lcdt
+  cleat
+  echo "✅ 更新已经完成~"
 }
 
 # 删除脚本
 delete_script() {
   # 读取用户输入
-  read -pr "是否彻底删除此脚本(y/n): " choice
+  read -rp "是否彻底删除此脚本(y/n)：" choice
   case $choice in
   Y | y)
     clear
@@ -142,19 +192,29 @@ delete_script() {
 
 # 不在 LobeChat 根目录
 no_lobechat() {
-  echo "❌ 当前不在 LobeChat 的根目录"
+  echo "❌ 当前不在 LobeChat 的根目录！"
   # 读取用户输入
-  read -pr "是否从 Git 克隆 LobeChat (y/n): " choice
+  read -rp "是否从 Git 克隆 LobeChat (y/n)：" choice
   case $choice in
   Y | y)
     clear
     # 读取用户输入
     DEFAULT_CLONE_PATH="/www/wwwroot"
-    read -pr "您要将 LobeChat 克隆到哪个目录，默认：$DEFAULT_CLONE_PATH" CLONE_PATH
-    # 如果没有输入，使用默认路径
-    if [ -z "$CLONE_PATH" ]; then
-      CLONE_PATH="$DEFAULT_CLONE_PATH"
-    fi
+    echo "您要将 LobeChat 克隆到哪个目录？"
+    # 使用正则表达式判断输入是否看似一个目录路径
+    while true; do
+      read -rp "默认：$DEFAULT_CLONE_PATH：" CLONE_PATH
+      # 如果没有输入，使用默认路径
+      if [ -z "$CLONE_PATH" ]; then
+        CLONE_PATH="$DEFAULT_CLONE_PATH"
+      fi
+      if ! echo "$CLONE_PATH" | grep -Eq '^((\./|/)?[a-zA-Z0-9]+(/[a-zA-Z0-9]+)*/?)$'; then
+        echo "❌ 您输入的是：$CLONE_PATH 这不是一个正确的目录！"
+        echo "请输入一个正确的目录，如：$DEFAULT_CLONE_PATH"
+      else
+        break
+      fi
+    done
     # 输出确认
     echo -e "您的设置路径是：$CLONE_PATH"
     echo -e "正在从 xtiii/LobeChat 克隆..."
@@ -164,14 +224,14 @@ no_lobechat() {
       cd "$CLONE_PATH"
       git clone https://github.com/xtiii/LobeChat.git || true
     fi
-    mv -f "$SCRIPT_PATH" /www/wwwroot/LobeChat/"$SCRIPT_NAME"
+    mv -f "$SCRIPT_PATH" "$CLONE_PATH"/LobeChat/"$SCRIPT_NAME"
     cd "$CLONE_PATH"/LobeChat && ./"$SCRIPT_NAME"
     exit 0
     ;;
   *)
     clear
     echo "❌ 请确保当前目录为 LobeChat 的根目录！"
-    exit 0
+    exit 1
     ;;
   esac
 }
@@ -188,8 +248,10 @@ link() {
 init() {
   link
   while true; do
+    cd "$SCRIPT_DIR"
     # 获取当前工作目录
     CURRENT_DIR=$(pwd)
+    echo -e "当前用的是：$LOCATION"
     echo -e " LobeChat 所在目录：$CURRENT_DIR"
     echo -e " LobeChatDeployTools 所在目录：$SCRIPT_PATH"
     echo -e "如果你是第一次构建 \033[32mLobeChat\033[0m 请先 安装依赖 再 构建程序"
@@ -201,27 +263,32 @@ init() {
     echo -e " 6 -> 数据库备份"
     echo -e " 7 -> 更新此脚本"
     echo -e " 8 -> 删除此脚本"
+    echo -e " 9 -> \033[32mPostgreSQL\033[0m"
     echo -e " 0 -> 退出程序"
     echo -e "Ps: 在任意地方输入 \033[32mlcdt\033[0m 命令即可运行此脚本~"
 
     # 读取用户输入
-    read -pr "请输入待执行的编号: " choice
+    read -rp "请输入待执行的编号：" choice
 
     case $choice in
     1)
       clear
+      echo "开始安装依赖"
       install
       ;;
     2)
       clear
+      echo "开始构建程序"
       build
       ;;
     3)
       clear
       run
+      echo "开始运行程序"
       ;;
     4)
       clear
+      echo "开始更新程序，这可能需要一些时间！"
       update
       ;;
     5)
@@ -253,7 +320,7 @@ init() {
   done
 }
 
-# 检查当前目录
+# 检查当前目录是否为 LobeChat 的根目录
 if [ ! -d "$SRC_DIR" ] || [ ! -f "$PACKAGE_FILE" ]; then
   no_lobechat
 fi
