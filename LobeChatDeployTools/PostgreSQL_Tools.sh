c3d88abe81e62368c990b90adb775267501aa717
SoTime <zlt990828@126.com>
2024-12-05 00:22:34 +0800

    添加PostgreSQL自动化安装

--- LobeChatDeployTools/PostgreSQL_Tools.sh
+++ LobeChatDeployTools/PostgreSQL_Tools.sh
@@ -4,17 +4,15 @@ clear
 
 set -e # 如果任何命令返回非零值，则退出脚本
 
-# PostgreSQL 版本
-pgsql_version=16.1
-
-cn_down_url="https://mirrors.aliyun.com/postgresql/source/v${pgsql_version}/postgresql-${pgsql_version}.tar.gz"
-en_down_url="https://ftp.postgresql.org/pub/source/v${pgsql_version}/postgresql-${pgsql_version}.tar.gz"
-
 # 安装目录
-install_dir="/www/service/pgsql"
+INSTALL_DIR="/www/service/pgsql"
+# PostgreSQL 版本
+PGSQL_VERSION=16.1
+CN_DOWN_URL="https://mirrors.aliyun.com/postgresql/source/v${PGSQL_VERSION}/postgresql-${PGSQL_VERSION}.tar.gz"
+EN_DOWN_URL="https://ftp.postgresql.org/pub/source/v${PGSQL_VERSION}/postgresql-${PGSQL_VERSION}.tar.gz"
 
 # 选择默认源
-location="Default"
+LOCATION="Default"
 
 # 获取脚本所在的完整原始路径
 SCRIPT_PATH=$(realpath "$0")
@@ -24,65 +22,66 @@ SCRIPT_PATH=$(realpath "$0")
 SCRIPT_NAME=$(basename "$SCRIPT_PATH")
 
 # 通过 Ping 检测下载链接
-urlSelect() {
-  ALIYUN="mirrors.aliyun.com"
-  POSTGRE="ftp.postgresql.org"
+url_select() {
+  aliyun="mirrors.aliyun.com"
+  postgresql="ftp.postgresql.org"
 
   # Ping 的延迟限制
-  TIMEOUT=0.5
+  timeout=0.5
 
   # 异步执行Ping测试第一个URL并获取延迟
-  (ping -W $TIMEOUT -c 3 $ALIYUN | tail -1 | awk -F '/' '{print $5}' >latency1.txt) &
+  (ping -W $timeout -c 3 $aliyun | tail -1 | awk -F '/' '{print $5}' >latency1.txt) &
 
   # 异步执行Ping测试第二个URL并获取延迟
-  (ping -W $TIMEOUT -c 3 $POSTGRE | tail -1 | awk -F '/' '{print $5}' >latency2.txt) &
+  (ping -W $timeout -c 3 $postgresql | tail -1 | awk -F '/' '{print $5}' >latency2.txt) &
 
   echo -e "正在为您选择源..."
   # 等待所有异步进程完成
   wait
 
   # 读取延迟值
-  ALIYUN_LATENCY=$(cat latency1.txt)
-  POSTGRE_LATENCY=$(cat latency2.txt)
+  aliyun_latency=$(cat latency1.txt)
+  postgresql_latency=$(cat latency2.txt)
 
   # 清理临时文件
   rm -rf latency1.txt latency2.txt
 
   # 比较两个URL的平均延迟
-  if (($(echo "$ALIYUN_LATENCY < $POSTGRE_LATENCY" | bc -l))); then
-    location="Aliyun"
+  if (($(echo "$aliyun_latency < $postgresql_latency" | bc -l))); then
+    LOCATION="Aliyun"
     sed -i 's/^LOCATION="Default"$/LOCATION="Aliyun"/' $SCRIPT_NAME
-    down_url=$cn_down_url
+    DOWN_URL=$CN_DOWN_URL
   else
-    location="Postgre"
-    sed -i 's/^LOCATION="Default"$/LOCATION="Postgre"/' $SCRIPT_NAME
-    down_url=$en_down_url
+    LOCATION="Postgre"
+    sed -i 's/^LOCATION="Default"$/LOCATION="PostgreSQL"/' $SCRIPT_NAME
+    DOWN_URL=$EN_DOWN_URL
   fi
 }
 
 # 自动选择国内外源
-if [[ $location == "Aliyun" ]]; then
-  down_url=$cn_down_url
-elif [[ $location == "Postgre" ]]; then
-  down_url=$en_down_url
+if [[ $LOCATION == "Aliyun" ]]; then
+  DOWN_URL=$CN_DOWN_URL
+elif [[ $LOCATION == "PostgreSQL" ]]; then
+  DOWN_URL=$EN_DOWN_URL
 else
-  urlSelect
+  url_select
 fi
 
 # 安装 PostgreSQL
 db_install() {
-  bash ./pgsql_install.sh $down_url $install_dir $pgsql_version
+  sudo bash ./pgsql_install.sh $DOWN_URL $INSTALL_DIR $PGSQL_VERSION
 }
 
 # 检查 PostgreSQL
-if [ "$(command -v psql)" ]; then
-  DB_VERSION=$(psql --version)
+if [ -f ${INSTALL_DIR}/bin/psql ]; then
+  DB_VERSION=$(su - postgres -c 'psql --version' | awk '{print $3}')
 else
   # 读取用户输入
   read -rp "PostgreSQL 未安装，是否安装(y/n)：" choice
   case $choice in
   Y | y)
     echo "正在为您安装 PostgreSQL..."
+    DB_VERSION=$PGSQL_VERSION
     db_install
     ;;
   *)
@@ -93,10 +92,8 @@ else
 fi
 
 init() {
-  echo -e "当前 PostgreSQL 版本：$DB_VERSION"
-  echo -e " LobeChat 所在目录：$CURRENT_DIR"
+  echo -e "当前 PostgreSQL 版本：${DB_VERSION}"
   echo -e " LobeChatDeployTools - PostgreSQL_Tools "
-  echo -e "如果你是第一次构建 \033[32mLobeChat\033[0m 请先 安装依赖 再 构建程序"
   echo -e " 1 -> 安装 PostgreSQL"
   echo -e " 2 -> 构建程序"
   echo -e " 3 -> 运行程序"
--- LobeChatDeployTools/pgsql_install.sh
+++ LobeChatDeployTools/pgsql_install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+set -e # 开启错误中止模式
+
+trap 'echo "Error occurred at line $LINENO: $BASH_COMMAND"' ERR
+
 #pgsql安装脚本
-down_url=$1
-install_dir=$2
-pgsql_version=$3
+DOWN_URL=$1
+INSTALL_DIR=$2
+PGSQL_VERSION=$3
+
+USERNAME="postgres"
+PASSWORD="pg123456"
 
 getCpuStat() {
   time1=$(cat /proc/stat | grep 'cpu ')
@@ -31,8 +38,8 @@ else
   cpuCore="1"
 fi
 
-loongarch64Check=$(uname -a | grep loongarch64)
-if [ "${loongarch64Check}" ]; then
+loongarch64Check=$(uname -m | grep -q loongarch64 && echo true || echo false)
+if [ "${loongarch64Check}" = "true" ]; then
   loongarch64_dis="--disable-spinlocks"
   loongarch64_build="--build=arm-linux"
 fi
@@ -58,26 +65,42 @@ fi
 
 cd /usr/local/src
 rm -rf post*
-wget $down_url
-if [ $? == 0 ]; then
-  tar -zxf "postgresql-${pgsql_version}.tar.gz" -C /usr/local/
+wget $DOWN_URL
+if [ $? -eq 0 ]; then
+  tar -xzvf "postgresql-${PGSQL_VERSION}.tar.gz" -C /usr/local/
 fi
 
 echo "pgsql文件解压成功"
 #判断用户是否存在
-user=postgres
-group=postgres
+user=${USERNAME}
+group=${USERNAME}
 
-#create group if not exists
-egrep "^$group" /etc/group >&/dev/null
-if [ $? -ne 0 ]; then
-  groupadd $group
+# Ensure $group is set and non-empty
+if [ -z "$group" ]; then
+  echo "Error: group variable is empty."
+  exit 1
 fi
 
-#create user if not exists
-egrep "^$user" /etc/passwd >&/dev/null
-if [ $? -ne 0 ]; then
-  useradd -m $user -g $group
+# Ensure /etc/group file is readable
+if [ ! -r /etc/group ]; then
+  echo "Error: /etc/group file is not readable or does not exist."
+  exit 1
+fi
+
+# 如果组不存在，则创建该组
+if ! grep -E "^$group:" /etc/group >/dev/null 2>&1; then
+  echo "Group $group does not exist. Creating group..."
+  groupadd "$group"
+else
+  echo "Group $group already exists."
+fi
+
+# 如果用户不存在，则创建用户
+if ! grep -E "^$user:" /etc/passwd >/dev/null 2>&1; then
+  echo "User $user does not exist. Creating user..."
+  useradd -m "$user" -g "$group"
+else
+  echo "User $user already exists."
 fi
 
 echo "重命名postgresql并且进入安装目录"
@@ -85,10 +108,15 @@ mv /usr/local/post* /usr/local/pgsql
 cd /usr/local/pgsql
 #-------------------------------安装pgsql------------------------------------
 echo "安装用得到的库文件及依赖"
-sudo apt install -y gcc make pkg-config libicu-dev zlib1g-dev >&/dev/null
+sudo apt update
+sudo apt install -y gcc make pkg-config libicu-dev zlib1g-dev
 echo "开始执行configure配置"
-mkdir -p $install_dir
-./configure --prefix=$install_dir --without-readline ${loongarch64_dis} ${loongarch64_build}
+# 判断${INSTALL_DIR}是否存在
+if [ -d ${INSTALL_DIR} ]; then
+  rm -rf ${INSTALL_DIR}
+fi
+mkdir -p ${INSTALL_DIR}
+./configure --prefix=${INSTALL_DIR} --without-readline ${loongarch64_dis} ${loongarch64_build}
 if [ $? == 0 ]; then
   echo "configure配置通过，开始进行make编译"
   make -j $cpuCore
@@ -106,64 +134,124 @@ else
   echo "configure检查配置失败，请查看错误进行安装库文件"
 fi
 echo "开始进行pgsql的配置"
+# 判断data是否存在
+if [ -d ${INSTALL_DIR}/data ]; then
+  rm -rf ${INSTALL_DIR}/data
+fi
 echo "给pgsql创建data目录"
-mkdir -p ${install_dir}/data
-mkdir -p ${install_dir}/logs
+mkdir -p ${INSTALL_DIR}/data
+mkdir -p ${INSTALL_DIR}/logs
 echo "修改用户组"
-chown -R postgres:postgres ${install_dir}
-chmod -R 700 ${install_dir}/data
+chown -R ${USERNAME}:${USERNAME} ${INSTALL_DIR}
+chmod -R 700 ${INSTALL_DIR}/data
 
 echo "添加环境变量,进入postgres用户目录"
-cd /home/postgres
-if [ -f /home/postgres/.bashrc ]; then
-  /bin/cp /home/postgres/.bashrc /home/postgres/.bashrc.bak
-  echo "export PGHOME=${install_dir}" >>/home/postgres/.bashrc
-  echo "export PGDATA=${install_dir}/data" >>/home/postgres/.bashrc
-  echo "export PG_CONFIG=\$PGHOME/bin/pg_config" >>/home/postgres/.bashrc
-  echo "export PATH=${install_dir}/bin:\$PATH " >>/home/postgres/.bashrc
-  echo "MANPATH=\$PGHOME/share/man:\$MANPATH" >>/home/postgres/.bashrc
-  echo "LD_LIBRARY_PATH=\$PGHOME/lib:\$LD_LIBRARY_PATH" >>/home/postgres/.bashrc
-  source /home/postgres/.bashrc
+cd /home/${USERNAME}
+if [ -f /home/${USERNAME}/.bashrc ]; then
+  /bin/cp /home/${USERNAME}/.bashrc /home/${USERNAME}/.bashrc.bak
+  echo "export PGHOME=${INSTALL_DIR}" >>/home/${USERNAME}/.bashrc
+  echo "export PGDATA=${INSTALL_DIR}/data" >>/home/${USERNAME}/.bashrc
+  echo "export PG_CONFIG=\$PGHOME/bin/pg_config" >>/home/${USERNAME}/.bashrc
+  echo "export PATH=${INSTALL_DIR}/bin:\$PATH " >>/home/${USERNAME}/.bashrc
+  echo "MANPATH=\$PGHOME/share/man:\$MANPATH" >>/home/${USERNAME}/.bashrc
+  echo "LD_LIBRARY_PATH=\$PGHOME/lib:\$LD_LIBRARY_PATH" >>/home/${USERNAME}/.bashrc
+  source /home/${USERNAME}/.bashrc
 fi
-if [ -f /home/postgres/.profile ]; then
-  /bin/cp /home/postgres/.profile /home/postgres/.profile.bak
-  echo "export PGHOME=${install_dir}" >>/home/postgres/.profile
-  echo "export PGDATA=${install_dir}/data" >>/home/postgres/.profile
-  echo "export PG_CONFIG=\$PGHOME/bin/pg_config" >>/home/postgres/.profile
-  echo "export PATH=${install_dir}/bin:\$PATH " >>/home/postgres/.profile
-  echo "MANPATH=\$PGHOME/share/man:\$MANPATH" >>/home/postgres/.profile
-  echo "LD_LIBRARY_PATH=\$PGHOME/lib:\$LD_LIBRARY_PATH" >>/home/postgres/.profile
-  source /home/postgres/.profile
+if [ -f /home/${USERNAME}/.profile ]; then
+  /bin/cp /home/${USERNAME}/.profile /home/${USERNAME}/.profile.bak
+  echo "export PGHOME=${INSTALL_DIR}" >>/home/${USERNAME}/.profile
+  echo "export PGDATA=${INSTALL_DIR}/data" >>/home/${USERNAME}/.profile
+  echo "export PG_CONFIG=\$PGHOME/bin/pg_config" >>/home/${USERNAME}/.profile
+  echo "export PATH=${INSTALL_DIR}/bin:\$PATH " >>/home/${USERNAME}/.profile
+  echo "MANPATH=\$PGHOME/share/man:\$MANPATH" >>/home/${USERNAME}/.profile
+  echo "LD_LIBRARY_PATH=\$PGHOME/lib:\$LD_LIBRARY_PATH" >>/home/${USERNAME}/.profile
+  source /home/${USERNAME}/.profile
 fi
 
-alias pg_start='pg_ctl -D $PGDATA -l ${install_dir}/logs/pgsql.log start'
-alias ps_stop='pg_ctl -D $PGDATA -l ${install_dir}/logs/pgsql.log stop'
+alias pg_start="su - postgres -c 'pg_ctl -D \$PGDATA -l \$PGHOME/logs/pgsql.log start'"
+alias pg_stop="su - postgres -c 'pg_ctl -D \$PGDATA -l \$PGHOME/logs/pgsql.log stop'"
 
-echo "切换至postgres用户来初始化数据库"
-su - postgres -c "${install_dir}/bin/initdb -D ${install_dir}/data"
+echo "切换至 ${USERNAME} 用户来初始化数据库"
+su - ${USERNAME} -c "${INSTALL_DIR}/bin/initdb -D ${INSTALL_DIR}/data"
+if [ $? -eq 0 ]; then
+  echo "数据库初始化完成！"
+else
+  echo "数据库初始化失败！"
+fi
+
+# 启动 PostgreSQL
+echo "启动PostgreSQL..."
+su - ${USERNAME} -c 'pg_ctl -D $PGDATA -l $PGHOME/logs/pgsql.log start'
+if [ $? -eq 0 ]; then
+  echo "PostgreSQL正在运行..."
+else
+  echo "PostgreSQL启动失败！！！"
+fi
+
+echo "正在安装 pgvector..."
+cd /tmp
+git clone --branch v0.8.0 https://github.com/pgvector/pgvector.git
+if [ $? -eq 0 ]; then
+  chown -R ${USERNAME}:${USERNAME} pgvector
+  cd pgvector
+  echo "正在编译 pgvector..."
+  su - ${USERNAME} -c "cd /tmp/pgvector && make && make install"
+  if [ $? -eq 0 ]; then
+    echo "启用 pgvector..."
+    su - ${USERNAME} -c "psql -c 'CREATE EXTENSION vector;'"
+  fi
+fi
+
+# 修改默认账户密码
+su - ${USERNAME} -c "psql -c \"ALTER USER ${USERNAME} WITH PASSWORD '${PASSWORD}';\""
+# 检查命令执行状态
+if [ $? -eq 0 ]; then
+  echo "用户 ${USERNAME} 的密码已成功修改。"
+else
+  echo "密码修改失败！"
+fi
 
 echo "进行必要的配置"
-# 在指定范围内将 trust 替换为 md5
-sed -i '/# "local" is for Unix domain socket connections only/,$ {s|trust|md5|g;}' "${install_dir}/data/pg_hba.conf"
-# 在指定范围内将 127.0.0.1/32 替换为 0.0.0.0/0
-sed -i '/# "local" is for Unix domain socket connections only/,$ {s|127.0.0.1/32|0.0.0.0/0|g;}' "${install_dir}/data/pg_hba.conf"
-# 在指定范围内将 ::1/128 替换为 ::/0
-sed -i '/# "local" is for Unix domain socket connections only/,$ {s|::1/128|::/0|g;}' "${install_dir}/data/pg_hba.conf"
-sed -i "s/#ssl = off/ssl = off/g" "${install_dir}/data/postgresql.conf"
-sed -i "s/#port = 5432/port = 5432/g" "${install_dir}/data/postgresql.conf"
-sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" "${install_dir}/data/postgresql.conf"
+if [ -f "${INSTALL_DIR}/data/pg_hba.conf" ]; then
+  # 在指定范围内将 trust 替换为 md5
+  sed -i '/# "local" is for Unix domain socket connections only/,$ {s|trust|md5|g;}' "${INSTALL_DIR}/data/pg_hba.conf"
+  # 在指定范围内将 127.0.0.1/32 替换为 0.0.0.0/0
+  sed -i '/# "local" is for Unix domain socket connections only/,$ {s|127.0.0.1/32|0.0.0.0/0|g;}' "${INSTALL_DIR}/data/pg_hba.conf"
+  # 在指定范围内将 ::1/128 替换为 ::/0
+  sed -i '/# "local" is for Unix domain socket connections only/,$ {s|::1/128|::/0|g;}' "${INSTALL_DIR}/data/pg_hba.conf"
+else
+  echo "文件 ${INSTALL_DIR}/data/pg_hba.conf 不存在！"
+fi
 
-echo "启用慢查询SQL语句跟踪"
-cat >>${install_dir}/data/postgresql.conf <<EOF
+if [ -f "${INSTALL_DIR}/data/postgresql.conf" ]; then
+  sed -i "s|#ssl = off|ssl = off|g" "${INSTALL_DIR}/data/postgresql.conf"
+  sed -i "s|#port = 5432|port = 5432|g" "${INSTALL_DIR}/data/postgresql.conf"
+  sed -i "s|#listen_addresses = 'localhost'|listen_addresses = '*'|g" "${INSTALL_DIR}/data/postgresql.conf"
+  echo "启用慢查询SQL语句跟踪"
+  cat >>${INSTALL_DIR}/data/postgresql.conf <<EOF
 logging_collector = on
 log_destination = 'stderr'
-log_directory = '${install_dir}/logs'
+log_directory = '${INSTALL_DIR}/logs'
 log_filename = 'postgresql-%Y-%m-%d.log'
 log_statement = all
 log_min_duration_statement = 5000
 EOF
+else
+  echo "文件 ${INSTALL_DIR}/data/postgresql.conf 不存在！"
+fi
 
-su - postgres -c "${install_dir}/bin/postgres -D ${install_dir}/data >>${install_dir}/logs/pgsql.log 2>&1 &"
-echo "---------------------------------------------------------------------------------------"
+su - ${USERNAME} -c "${INSTALL_DIR}/bin/postgres -D ${INSTALL_DIR}/data >>${INSTALL_DIR}/logs/pgsql.log 2>&1 &"
+
+# 重启 PostgreSQL
+echo "重启PostgreSQL..."
+su - ${USERNAME} -c 'pg_ctl -D $PGDATA -l $PGHOME/logs/pgsql.log restart'
+if [ $? -eq 0 ]; then
+  echo "PostgreSQL正在运行..."
+else
+  echo "PostgreSQL重启失败！！！"
+fi
+
 echo "---------------------------------------------------------------------------------------"
 echo "----------------------------SUCCESS INSTALLATION OF POSTGRESQL-------------------------"
+echo "---------------------------------------------------------------------------------------"
+echo "------------------- 数据库用户名：${USERNAME} - 数据库密码：${PASSWORD} -------------------"
